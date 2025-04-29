de96e4dec4fd322d1ce78e347aa2cd7456fe0993
Carl Schwan <carl@carlschwan.eu>
2025-04-29 21:22:37 +0200

    Use SPDX headers for license information

--- lib/decompressor.cpp
+++ lib/decompressor.cpp
@@ -1,16 +1,8 @@
-/***************************************************************************
- *   Copyright (C) 2008 by Jakub Stachowski <qbast@go2.pl>                 *
- *                                                                         *
- *   RLE decompressor based on FBReader                                    *
- *   Copyright (C) 2004-2008 Geometer Plus <contact@geometerplus.com>      *
- *                                                                         *
- *   Huffdic decompressor based on Python code by Igor Skochinsky          *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
+// SPDX-FileCopyrightText: 2008 by Jakub Stachowski <qbast@go2.pl>
+// RLE decompressor based on FBReader
+// SPDX-FileCopyrightText: 2004-2008 Geometer Plus <contact@geometerplus.com>
+// Huffdic decompressor based on Python code by Igor Skochinsky
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "decompressor.h"
 #include "mobipocket.h"
