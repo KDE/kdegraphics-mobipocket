8cc4173be882eb99de1a48e45b6d7c13281d623b
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2025-06-12 22:27:51 +0200

    Extend BitReader tests and benchmark
    Extend the coverage to code lengths which do not fit into a byte exactly.

--- autotests/bitreadertest.cpp
+++ autotests/bitreadertest.cpp
@@ -20,10 +20,12 @@ private Q_SLOTS:
     void testRead_2();
     void testRead1Bit();
     void testRead8Bit();
+    void testRead16Bit();
+    void testRead12Bit();
     void benchmarkInit();
     void benchmarkInitSlice();
-    void benchmarkRead1Bit();
-    void benchmarkRead8Bit();
+    void benchmarkRead();
+    void benchmarkRead_data();
 };
 
 void BitReaderTest::testRead_1()
@@ -139,6 +141,57 @@ void BitReaderTest::testRead8Bit()
     QVERIFY(!r.eat(1));
 }
 
+void BitReaderTest::testRead16Bit()
+{
+    QByteArray data(1024, '\x01');
+
+    size_t count = 0;
+    BitReader r(data);
+    while (r.left() > 24) {
+        count++;
+        auto t = r.read();
+        QCOMPARE(t, 0x01010101);
+        r.eat(16);
+    }
+    QCOMPARE(count, 511);
+
+    QCOMPARE(r.read(), 0x01010000);
+    QVERIFY(r.eat(16));
+    QCOMPARE(r.left(), 0);
+    QCOMPARE(r.read(), 0x00000000);
+    QVERIFY(!r.eat(1));
+}
+
+void BitReaderTest::testRead12Bit()
+{
+    QByteArray data(304, '\x01');
+
+    BitReader r(data);
+
+    size_t count = 0;
+    while (r.left() > 36) {
+        count++;
+        auto t = r.read();
+        QCOMPARE(t, 0x01010101);
+        QVERIFY(r.eat(12));
+        t = r.read();
+        QCOMPARE(t, 0x10101010);
+        QVERIFY(r.eat(12));
+    }
+    QCOMPARE(count, 100);
+
+    QCOMPARE(r.left(), 32);
+    QCOMPARE(r.read(), 0x01010101);
+    QVERIFY(r.eat(12));
+    QCOMPARE(r.left(), 20);
+    QCOMPARE(r.read(), 0x10101000);
+    QVERIFY(r.eat(12));
+    QCOMPARE(r.left(), 8);
+    QCOMPARE(r.read(), 0x01000000);
+    QVERIFY(r.eat(8));
+    QVERIFY(!r.eat(1));
+}
+
 void BitReaderTest::benchmarkInit()
 {
     QByteArray data(1024, '\0');
@@ -157,30 +210,32 @@ void BitReaderTest::benchmarkInitSlice()
     }
 }
 
-void BitReaderTest::benchmarkRead1Bit()
+void BitReaderTest::benchmarkRead()
 {
-    QByteArray data(128, '\0');
+    QFETCH(QByteArray, data);
+    QFETCH(int, count);
 
+    qint64 t = 0;
     QBENCHMARK {
         BitReader r(data);
-        while (r.left()) {
-            r.read();
-            r.eat(1);
+        while (r.left() > 0) {
+            t += r.read();
+            r.eat(count);
         }
     }
+    QVERIFY(t > (data.size() / count));
 }
 
-void BitReaderTest::benchmarkRead8Bit()
+void BitReaderTest::benchmarkRead_data()
 {
-    QByteArray data(1024, '\0');
+    QTest::addColumn<QByteArray>("data");
+    QTest::addColumn<int>("count");
 
-    QBENCHMARK {
-        BitReader r(data);
-        while (r.left()) {
-            r.read();
-            r.eat(8);
-        }
-    }
+    QTest::addRow("1") << QByteArray(1024, '\x01') << 1;
+    QTest::addRow("4") << QByteArray(1024, '\x01') << 4;
+    QTest::addRow("8") << QByteArray(1024, '\x01') << 8;
+    QTest::addRow("12") << QByteArray(1024, '\x01') << 12;
+    QTest::addRow("7") << QByteArray(1024, '\x01') << 7;
 }
 
 QTEST_GUILESS_MAIN(BitReaderTest)
